1243bb284b76eb336d1aa308ac97672f7df1e47f
ug <ug.gaming.tv@gmail.com>
2021-09-15 23:58:01 +0800

    add multiple save logic

--- contentScript.js
+++ contentScript.js
@@ -3,31 +3,53 @@ $(function() {
     const wrapper = '<div id="TN_Buttons"></div>'
     const saveButton = '<button type="button" class="TN-button -save" id="TN_SaveTranslateButton">SAVE</button>';
     const loadButton = '<button type="button" class="TN-button -load" id="TN_LoadTranslateButton">LOAD</button>';
+    const deleteButton = '<button type="button" class="TN-button -delete" id="TN_DeleteTranslateButton">DELETE</button>';
     $("body").append(wrapper);
+    $(deleteButton).prependTo("#TN_Buttons");
     $(loadButton).prependTo("#TN_Buttons");
     $(saveButton).prependTo("#TN_Buttons");
 
     // 左：日本語　右：英語　の場合
     $("#TN_SaveTranslateButton").on("click", function() {
+        var hashKey = Math.random().toString(32).substring(2);
         var japanese = $("textarea[aria-label='原文'] + div").text();
         var english = $("span[data-language-for-alternatives] > span").text();
-    
-        console.log(japanese);
-        console.log(english);
-        chrome.storage.sync.set(
-            {
-                "TN_1": { 
-                    "ja" : japanese, 
-                    "en" : english
-                }
+
+        var lang = {
+            "ja": japanese, 
+            "en": english
+        };
+        var textItem = {};
+
+        chrome.storage.sync.get(["TN"], function(items) {
+            // Check data
+            if (typeof items.TN === 'undefined') {
+                textItem[hashKey] = lang;
+                console.log("none => ");
+                console.log(textItem);
+            } else {
+                textItem = items.TN;
+                console.log("pushed 1 => ");
+                console.log(textItem);
+                textItem[hashKey] = lang;
+                console.log("pushed 2 => ");
+                console.log(textItem);
             }
-        );
+            // Save
+            chrome.storage.sync.set({"TN": textItem});
+        });
     });
 
+    // デバッグ用
     $("#TN_LoadTranslateButton").on("click", function() {
-        chrome.storage.sync.get(["TN_1"], function(items) {
-            console.log(items.TN_1);
+        chrome.storage.sync.get(["TN"], function(items) {
+            console.log(items);
         });
+    });
+
+    $("#TN_DeleteTranslateButton").on("click", function() {
+        chrome.storage.sync.clear();
+        console.log("ALL DELETE");
     });
 
 });
--- popup.js
+++ popup.js
@@ -1,10 +1,20 @@
 
+document.body.onload = function() {
+    chrome.storage.sync.get(["TN"], function(items) {
+      if (!chrome.runtime.error) {
+        console.log(items);
+        document.getElementById("TN-list").innerText = items.data;
+      }
+    });
+  }
+
+  
 $(function() {
-    chrome.storage.sync.get(["TN_1"], function(items) {
-        console.log(items.TN_1);
-        let itemJa = '<div class="text-item">' + items.TN_1.ja + '</div>';
-        let itemEn = '<div class="text-item">' + items.TN_1.en + '</div>';
-        $(itemJa).prependTo("#TN-list");
-        $(itemEn).prependTo("#TN-list");
+    chrome.storage.sync.get(["TN"], function(items) {
+        console.log(items.TN);
+        // let itemJa = '<div class="text-item">' + items.TN_1.ja + '</div>';
+        // let itemEn = '<div class="text-item">' + items.TN_1.en + '</div>';
+        // $(itemJa).prependTo("#TN-list");
+        // $(itemEn).prependTo("#TN-list");
     });
 });
