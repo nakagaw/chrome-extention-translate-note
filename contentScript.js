3b0a48d4c68daca33c798fc69f7b9c27b3f2a595
ug <ug.gaming.tv@gmail.com>
2021-09-25 14:12:28 +0800

    add delete feature and grammarly workarround

--- contentScript.js
+++ contentScript.js
@@ -11,9 +11,19 @@ $(function() {
 
     $("#TN_SaveTranslateButton").on("click", function() {
         const hashKey = Math.random().toString(32).substring(2);
-        const mode = $("span[data-language-to-translate-into]").attr("data-language-to-translate-into");
-        const originLang = $("textarea[aria-label='原文'] + div").text();
-        const altLang = $("span[data-language-for-alternatives] > span").text();
+        let mode = $("span[data-language-to-translate-into]").attr("data-language-to-translate-into");
+        let grammarlyCheck = $("grammarly-extension").length;
+        let originLang;
+        let altLang;
+        if(grammarlyCheck) {
+            console.log("+g");
+            originLang = $("textarea[aria-label='原文'] + grammarly-extension + grammarly-extension + div").text();
+            altLang = $("span[data-language-for-alternatives] > span").text();
+        } else {
+            console.log("-g");
+            originLang = $("textarea[aria-label='原文'] + div").text();
+            altLang = $("span[data-language-for-alternatives] > span").text();
+        }
         let lang = {};
         let textItem = {};
 
@@ -45,30 +55,46 @@ $(function() {
 
     // デバッグ用
     $("#TN_LoadTranslateButton").on("click", function() {
-        chrome.storage.sync.get(["TN"], function(TN) {
-            if (!Object.keys(TN).length) return; // undefinedチェック
-            
-            let obj = Object.values(TN);
-            let idList = Object.keys(obj[0]); // 配列から配列のみ取り出し
-            let ul = document.createElement('ul');
-            ul.className = "TN-list";
-            for (let value of idList) {
-                // データひとつずつの取り出しに成功 => obj[0][value]
-                const li = document.createElement('li');
-                let lang1div = document.createElement('div');
-                let lang2div = document.createElement('div');
+        var grammarlyCheck = $("grammarly-extension").length;
+        console.log(grammarlyCheck);
+        let originLang;
+        let altLang;
+        if(grammarlyCheck) {
+            console.log("あり");
+            originLang = $("textarea[aria-label='原文'] + grammarly-extension + grammarly-extension + div").text();
+            altLang = $("span[data-language-for-alternatives] > span").text();
+        } else {
+            console.log("なし");
+            originLang = $("textarea[aria-label='原文'] + div").text();
+            altLang = $("span[data-language-for-alternatives] > span").text();
+        }
+        console.log(originLang);
+        console.log(altLang);
 
-                li.setAttribute("id", value);
-                li.className = "TN-list__item";
-                lang1div.className = "lang1";
-                lang2div.className = "lang2";
-                lang1div.innerHTML = obj[0][value]["lang1"];
-                lang2div.innerHTML = obj[0][value]["lang2"];
-                li.append(lang1div,lang2div);
-                ul.append(li);
-            }
-            console.log(ul);
-        });
+        // chrome.storage.sync.get(["TN"], function(TN) {
+        //     if (!Object.keys(TN).length) return; // undefinedチェック
+            
+        //     let obj = Object.values(TN);
+        //     let idList = Object.keys(obj[0]); // 配列から配列のみ取り出し
+        //     let ul = document.createElement('ul');
+        //     ul.className = "TN-list";
+        //     for (let value of idList) {
+        //         // データひとつずつの取り出しに成功 => obj[0][value]
+        //         const li = document.createElement('li');
+        //         let lang1div = document.createElement('div');
+        //         let lang2div = document.createElement('div');
+
+        //         li.setAttribute("id", value);
+        //         li.className = "TN-list__item";
+        //         lang1div.className = "lang1";
+        //         lang2div.className = "lang2";
+        //         lang1div.innerHTML = obj[0][value]["lang1"];
+        //         lang2div.innerHTML = obj[0][value]["lang2"];
+        //         li.append(lang1div,lang2div);
+        //         ul.append(li);
+        //     }
+        //     console.log(ul);
+        // });
     });
 
     $("#TN_DeleteTranslateButton").on("click", function() {
--- popup.js
+++ popup.js
@@ -1,41 +1,66 @@
-document.body.onload = function() {
-  chrome.storage.sync.get(["TN"], function(TN) {
+$(function() {
+  //initDOM
+  chrome.storage.sync.get(["TN"], function(items) {
     if (!chrome.runtime.error) {
-      if (!Object.keys(TN).length) return; // undefinedチェック
-      
-      let obj = Object.values(TN);
-      let idList = Object.keys(obj[0]); // 配列から配列のみ取り出し
-      let ul = document.createElement('ul');
-      ul.className = "TN-list";
-      for (let value of idList) {
-        // データひとつずつの取り出しに成功 => obj[0][value]
-        const li = document.createElement('li');
-        let lang1div = document.createElement('textarea');
-        let lang2div = document.createElement('textarea');
-  
-        li.setAttribute("id", value);
-        li.className = "TN-list__item";
-        lang1div.className = "TN-list__item-lang -lang1";
-        lang2div.className = "TN-list__item-lang -lang2";
-        lang1div.innerHTML = obj[0][value]["lang1"];
-        lang2div.innerHTML = obj[0][value]["lang2"];
-        li.append(lang1div,lang2div);
-        ul.append(li);
-      }
-      console.log(ul);
-      document.getElementById("TN").append(ul);
+      createDOM(items)
     }
   });
-}
 
-$(function(){
-  $('.TN-list__item-lang textarea')
-  .on('input', function(){
-    if ($(this).outerHeight() > this.scrollHeight){
-      $(this).height(1)
-    }
-    while ($(this).outerHeight() < this.scrollHeight){
-      $(this).height($(this).height() + 1)
-    }
+  //deleteItem
+  $("#TN").on('click', '.js_delete', function(e) {
+    let _id = e.currentTarget.parentNode.id;
+    console.log(_id); 
+    
+    chrome.storage.sync.get(["TN"], function(items) {
+      let textItem = {};
+      // Check data
+      if (typeof items.TN === 'undefined') {
+          textItem[hashKey] = lang; // undefinedチェック
+      } else {
+          textItem = items.TN //既存データ
+          console.log(textItem);
+          delete textItem[_id];
+          console.log(textItem);
+      }
+      //削除後のデータを保存して再描画
+      chrome.storage.sync.set({"TN": textItem});
+      chrome.storage.sync.get(["TN"], function(items) {
+        if (!chrome.runtime.error) {
+          createDOM(items)
+        }
+      });
+      window.location.reload();
+    });
   });
 });
+
+
+function createDOM(items) {
+  if (!Object.keys(items).length) return; // undefinedチェック
+  
+  let obj = Object.values(items);
+  let idList = Object.keys(obj[0]); // 配列から配列のみ取り出し
+  let ul = document.createElement('ul');
+  ul.className = "TN-list";
+  for (let value of idList) {
+    // データひとつずつの取り出しに成功 => obj[0][value]
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+    let lang1div = document.createElement('textarea');
+    let lang2div = document.createElement('textarea');
+
+    li.setAttribute("id", value);
+    li.className = "TN-list__item";
+    deleteButton.setAttribute("type", "button");
+    deleteButton.className = "TN-list__delete js_delete";
+    deleteButton.innerHTML = "x";
+    lang1div.className = "TN-list__item-lang -lang1";
+    lang2div.className = "TN-list__item-lang -lang2";
+    lang1div.innerHTML = obj[0][value]["lang1"];
+    lang2div.innerHTML = obj[0][value]["lang2"];
+    li.append(deleteButton,lang1div,lang2div);
+    ul.append(li);
+  }
+  console.log(ul);
+  document.getElementById("TN").append(ul);
+}
